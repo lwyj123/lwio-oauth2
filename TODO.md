bd5f08e38cc0ef3fc619c5da3b318ea733343390
lwyj123 <443474713@qq.com>
2018-06-22 23:27:06 +0800

    feat: finished oauth2 server and a demo client

---
+++ TODO.md
@@ -0,0 +1,2 @@
+1. translate prototype part into es6/es7 class
+2. use real database instead of fake db.
---
+++ src/db/fake.js
@@ -0,0 +1,17 @@
+const MemoryStorage = require('simple-memory-storage');
+
+const db = new MemoryStorage();
+
+//pre-store an user for the example
+db.set('lwio', {
+    'username': 'lwio',
+    'password': 'lwio',
+    'firstName': 'Yuyi',
+    'lastName': 'Liang',
+    'hobbies': [ 'coding', 'music' ]
+});
+
+/**
+ * we don't use real database in this example
+ */
+module.exports = db;
